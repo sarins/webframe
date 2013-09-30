2a65a1d19ac7d1fd614f38fb4c5838744fa0137f
sarins <axl.zhao@gmail.com>
2013-09-30 13:31:35 +0800

    formatted.

--- webframe/src/com/huatek/framework/entity/FwSystem.java
+++ webframe/src/com/huatek/framework/entity/FwSystem.java
@@ -2,13 +2,12 @@ package com.huatek.framework.entity;
 
 import com.huatek.base.entity.NamedEntity;
 
-public class FwSystem extends NamedEntity implements java.io.Serializable{
+public class FwSystem extends NamedEntity implements java.io.Serializable {
 
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = 1155807352468031510L;
-
 
 	/** nullable persistent field */
 	private String describe;
@@ -21,66 +20,56 @@ public class FwSystem extends NamedEntity implements java.io.Serializable{
 
 	private String contextPath;
 
-
 	public String getDescribe() {
 		return describe;
 	}
-
 
 	public void setDescribe(String describe) {
 		this.describe = describe;
 	}
 
-
 	public String getSysCode() {
 		return sysCode;
 	}
-
 
 	public void setSysCode(String sysCode) {
 		this.sysCode = sysCode;
 	}
 
-
 	public String getSysIp() {
 		return sysIp;
 	}
-
 
 	public void setSysIp(String sysIp) {
 		this.sysIp = sysIp;
 	}
 
-
 	public Integer getSysPort() {
 		return sysPort;
 	}
-
 
 	public void setSysPort(Integer sysPort) {
 		this.sysPort = sysPort;
 	}
 
-
 	public String getContextPath() {
 		return contextPath;
 	}
-
 
 	public void setContextPath(String contextPath) {
 		this.contextPath = contextPath;
 	}
 
-	public String getSystemURL(String localIp,Integer port){
-		if(this.sysIp==null){
-			if(this.sysPort==null){
-				return "http://"+localIp+":"+port+this.contextPath;
-			}else{
-				return "http://"+localIp+":"+this.sysPort+this.contextPath;
+	public String getSystemURL(String localIp, Integer port) {
+		if (this.sysIp == null) {
+			if (this.sysPort == null) {
+				return "http://" + localIp + ":" + port + this.contextPath;
+			} else {
+				return "http://" + localIp + ":" + this.sysPort
+						+ this.contextPath;
 			}
 		}
-		return "http://"+this.sysIp+":"+this.sysPort+this.contextPath;
+		return "http://" + this.sysIp + ":" + this.sysPort + this.contextPath;
 	}
-
 
 }
